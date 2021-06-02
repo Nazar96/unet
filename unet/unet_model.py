1a17bac2b6e33e20d4b5cca4af60e0e04c7aee29
Тёхта Назарий Александрович <Tekhta.N.A@sberbank.ru>
2021-06-02 14:52:07 +0300

    Added custom_unet class constructor

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -3,6 +3,7 @@
 import torch.nn.functional as F
 
 from .unet_parts import *
+from typing import *
 
 
 class UNet(nn.Module):
@@ -23,6 +24,86 @@ class UNet(nn.Module):
         self.up3 = Up(256, 128 // factor, bilinear)
         self.up4 = Up(128, 64, bilinear)
         self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+
+
+def custom_unet(
+    input_shape,
+    num_classes=1,
+    activation="relu",
+    use_batch_norm=True,
+    upsample_mode="deconv",  # 'deconv' or 'simple'
+    dropout=0.3,
+    dropout_change_per_layer=0.0,
+    dropout_type="spatial",
+    use_dropout_on_upsampling=False,
+    use_attention=False,
+    filters=16,
+    num_layers=4,
+    output_activation="sigmoid",
+):  # 'sigmoid' or 'softmax'
+    pass
+
+
+class CustomUNet(nn.Module):
+    def __init__(
+            self,
+            num_channels: int = 1,
+            num_classes: int = 1,
+            activation: str = 'relu',
+            use_batch_norm: bool = True,
+            dropout: float = .3,
+            dropout_change_per_layer: float = .3,
+            dropout_type: str = 'spatial',
+            use_attention: bool = False,
+            filters: int = 16,
+            num_layers: int = 4,
+            output_activation: str = 'sigmoid',
+            bilinear: bool = True,
+    ):
+        super(CustomUNet, self).__init__()
+        self.num_channels = num_channels
+        self.num_classes = num_classes
+        self.bilinear = bilinear
+        self.num_layers = num_layers
+        self.filters = filters
+
+        self.inc = DoubleConv(self.num_channels, self.filters)
+        self.outc = OutConv(self.filters, self.num_classes)
+
+        self.down_list = []
+        self.up_list = []
+
+        factor = 2
+        in_channels = self.filters
+        for i in range(self.num_layers):
+            self.down_list.append(Down(in_channels, in_channels * factor))
+            self.up_list.append(Up(in_channels * factor, in_channels))
+            in_channels *= factor
+
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+
+
 
     def forward(self, x):
         x1 = self.inc(x)
